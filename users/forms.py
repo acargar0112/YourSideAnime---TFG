72ead5e4ca056bc749426d941a827092bfc1c64f
acargar0112 <acargar0112@fp.iesromerovargas.com>
2026-05-06 18:52:25 +0200

    He añadido comentarios a las siguientes apps: core y users | Y comentario al nginx.conf

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,10 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    """
+    Formulario personalizado para registrar usuarios.
+    """
     class Meta:
         model = CustomUser
+        # Campos que se mostrarán en el formulario de registro.
         fields = ["username", "email", "password1", "password2" ,"avatar", "bio"]
--- users/models.py
+++ users/models.py
@@ -4,16 +4,26 @@ from django.db import models
 from django.core.validators import FileExtensionValidator
 
 def validar_tamano_avatar(archivo):
+    """
+    Validador personalizado para limitar el tamaño del avatar a 2 MB.
+    En caso de que este lo supere se lanzará un ValidationError.
+    """
     limite = 2
     if archivo.size > limite * 1024 * 1024:
         raise ValidationError(f"El avatar no puede superar los {limite} MB.")
 
 
 class CustomUser(AbstractUser):
+    """
+    Modelo de usuario personalizado que extende el AbstractUser, en este caso hemos añadido los campos avatar y bio.
+    """
     avatar = models.ImageField(upload_to="avatars/", blank=True, null=True, validators=[FileExtensionValidator(["jpg","jpeg","png"]), validar_tamano_avatar])
     bio = models.TextField(blank=True, null=True)
 
     def clean(self):
+        """
+        Validación para el email introducido para el registro del usuario, si este tiene un fallo lanza un ValidationError.
+        """
         errors = {}
 
         if self.email:
@@ -34,4 +44,7 @@ class CustomUser(AbstractUser):
 
 
     def __str__(self):
+        """
+        Representación en el admin
+        """
         return self.username
