df4445f0b1465b49a543c8f43a33653285ae967a
acargar0112 <acargar0112@fp.iesromerovargas.com>
2026-05-08 14:22:06 +0200

    Se ha importado algo sin querer que no voy a utilizar, lo borro.

--- animes/views.py
+++ animes/views.py
@@ -1,5 +1,3 @@
-from wsgiref.util import application_uri
-
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
